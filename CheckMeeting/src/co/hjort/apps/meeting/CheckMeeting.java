66387d1af5a81ca1e964676a7229701a653791a2
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2012-02-22 17:52:00 -0200

    Criado projeto CheckMeeting.

---
+++ CheckMeeting/src/co/hjort/apps/meeting/CheckMeeting.java
@@ -0,0 +1,13 @@
+package co.hjort.apps.meeting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckMeeting extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
